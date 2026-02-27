372090808eb36a219363c8e175a9f186cad839a7
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-02-27 19:11:28 +0200

    Update lottery.c

--- lab3/code/lottery.c
+++ lab3/code/lottery.c
@@ -2,52 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 #include <signal.h>
+#include <sys/resource.h>
 #include <unistd.h>
 
 
-void on_cpu_limit(int sig) {
-	(void)sig;
-	const char msg[] = "\nCPU time limit reached! Program stopped correctly. \n";
-	write(1, msg, sizeof(msg) - 1);
-	_exit(0);
+void cpu_limit_handler(int sig) {
+    (void)sig;
+    printf("\nCPU time limit reached! Program stopped.\n");
+    fflush(stdout);
+    exit(0);
 }
 
-void pick_unique(int *out, int k, int max) {
-	int used[50] = {0};
-	int count = 0;
-	
-	while (count < k) {
-		int x = (rand() % max) + 1;
-		if (!used[x]) {
-			used[x] = 1;
-			out[count] = x;
-			count++;
-		}
-	}
+
+void generate_numbers(int arr[], int k, int max) {
+    int used[50] = {0};
+    int count = 0;
+
+    while (count < k) {
+        int num = rand() % max + 1;   // число 1..max
+        if (!used[num]) {
+            used[num] = 1;
+            arr[count] = num;
+            count++;
+        }
+    }
 }
 
-int main(void) {
-	signal(SIGXCPU, on_cpu_limit);
-	srand((unsigned)time(NULL));
-	
-	int a[7], b[6];
-	while (1) {
-		pick_unique(a, 7, 49);
-		pick_unique(b, 6, 36);
-		
-		printf("7/49: ");
-		for (int i = 0; i < 7; i++) printf("%d ", a[i]);
-		printf("\n");
-		
-		printf("6/36: ");
-		for (int i = 0; i < 6; i++) printf("%d ", b[i]);
-		printf("\n\n");
-		
-		volatile unsigned long long s = 0;
-		for (unsigned long long i = 0; i < 50000000ULL; i++){
-			s += 1;
-		}
-	}
+int main() {
+    struct rlimit limit;
+    limit.rlim_cur = 1;  // soft limit
+    limit.rlim_max = 2;  // hard limit
+    setrlimit(RLIMIT_CPU, &limit);
 
-	return 0;
+    signal(SIGXCPU, cpu_limit_handler);
+
+    srand(time(NULL));
+
+    int a[7], b[6];
+
+    while (1) {
+        generate_numbers(a, 7, 49);
+        generate_numbers(b, 6, 36);
+
+        printf("7/49: ");
+        for (int i = 0; i < 7; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+
+        printf("6/36: ");
+        for (int i = 0; i < 6; i++)
+            printf("%d ", b[i]);
+        printf("\n\n");
+
+        volatile unsigned long long x = 0;
+        for (unsigned long long i = 0; i < 20000000ULL; i++)
+            x += i;
+    }
+
+    return 0;
 }
