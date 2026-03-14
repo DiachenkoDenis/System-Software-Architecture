7e6a79a3882b69b6b3db05cad159be1470b4e96d
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-03-14 22:44:00 +0200

    Update malloc_test.c

--- lab4/code/malloc_test.c
+++ lab4/code/malloc_test.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-	printf("malloc test\n");
-	return 0;
+    size_t max_size = (size_t)-1;
+    printf("Maximum value of size_t: %zu\n", max_size);
+    void *ptr = malloc(max_size);
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+    } else {
+        printf("Memory allocated\n");
+        free(ptr);
+    }
+    return 0;
 }
