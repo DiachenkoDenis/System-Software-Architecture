6ffe02110054e41e552d50a57d48f864720f8f87
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-04-02 20:22:54 +0300

    Update task.c

--- lab6/code/task.c
+++ lab6/code/task.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+    char *ptr = malloc(32);
+
+    if (ptr == NULL) {
+        printf("malloc failed\n");
+        return 1;
+    }
+
+    strcpy(ptr, "Hello from heap memory");
+    printf("Before free: %s\n", ptr);
+
+    free(ptr);
+
+    ptr[0] = 'X';
+
+    printf("After free: %s\n", ptr);
+
+    return 0;
+}
