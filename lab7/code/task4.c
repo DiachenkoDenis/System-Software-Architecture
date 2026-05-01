f994b4bf47321a154d02406e42059641083e6e5d
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-05-01 22:54:12 +0300

    Create task4.c

---
+++ lab7/code/task4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024
+#define PAGE_SIZE 20
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    char line[MAX_LINE];
+    int line_count = 0;
+
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    file = fopen(argv[1], "r");
+
+    if (file == NULL) {
+        perror("fopen failed");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+        line_count++;
+
+        if (line_count % PAGE_SIZE == 0) {
+            printf("\n--- Press Enter to continue ---");
+            getchar();
+        }
+    }
+
+    fclose(file);
+
+    return 0;
+}
