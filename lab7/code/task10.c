8fded72e67edc91d257b31da606a4e053e35006d
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-05-02 00:01:59 +0300

    Create task10.c

---
+++ lab7/code/task10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    float n;
+
+    srand(time(NULL));
+
+    printf("Enter n: ");
+    scanf("%f", &n);
+
+    printf("\nRandom numbers in range [0.0, 1.0]:\n");
+    for (int i = 0; i < 5; i++) {
+        float r = (float)rand() / RAND_MAX;
+        printf("%f\n", r);
+    }
+
+    printf("\nRandom numbers in range [0.0, %f]:\n", n);
+    for (int i = 0; i < 5; i++) {
+        float r = ((float)rand() / RAND_MAX) * n;
+        printf("%f\n", r);
+    }
+
+    return 0;
+}
