1d2811d8a454cedb7a7444fc22f4bf2f49c14e53
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-05-03 17:04:27 +0300

    Update task8_4.c

--- lab8/code/task8_4.c
+++ lab8/code/task8_4.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
+int main(void) {
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid == -1) {
+        perror("fork failed");
+        return 1;
+    }
+
+    printf("fork returned: %d\n", pid);
+
+    if (pid == 0) {
+        printf("This is child process\n");
+    } else {
+        printf("This is parent process, child PID = %d\n", pid);
+    }
+
+    return 0;
+}
