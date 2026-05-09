86c26a83aa2ee20d5d5725282f6842fcb83b1241
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-05-09 22:28:38 +0300

    Create task9_1.c

---
+++ lab9/code/task9_1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_LENGTH 1024
+
+int main() {
+    FILE *file;
+    char line[MAX_LINE_LENGTH];
+    char *username, *uid_str;
+    int uid;
+    file = popen("getent passwd", "r");
+    if (file == NULL) {
+        perror("Не вдалося отримати дані з getent");
+        return 1;
+    }
+    char current_user[MAX_LINE_LENGTH];
+    getlogin_r(current_user, sizeof(current_user));
+    printf("Звичайні користувачі (окрім вас):\n");
+    while (fgets(line, sizeof(line), file)) {
+        // Розбиваємо рядок на частини
+        username = strtok(line, ":");
+        strtok(NULL, ":");  
+        uid_str = strtok(NULL, ":");
+        strtok(NULL, ":"); 
+        uid = atoi(uid_str);
+        if (uid >= 500 && strcmp(username, current_user) != 0) {
+            printf("%s (UID: %d)\n", username, uid);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
